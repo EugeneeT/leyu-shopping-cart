e1ff246ab6d244d050a7c3bc190d0eec7f2a6a47
EugeneeT <vina.botanic@gmail.com>
2023-11-03 11:32:20 +0100

    init

---
+++ shopping-cart.js
@@ -0,0 +1,9 @@
+
+// import the necessary module READLINE:
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const cart = [];
